596eb2afb547674e56743a63aa8b734873c6bb65
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2021-05-22 01:01:20 +0100

    update dependencies and scripts

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,9 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import dts from "rollup-plugin-dts";
 import fs from 'fs';
-import pkg from './package.json';
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import * as pkg from './package.json';
+import typescript from '@rollup/plugin-typescript';
 import { DEFAULT_EXTENSIONS } from '@babel/core';
 import { terser } from "rollup-plugin-terser";
 
@@ -42,9 +42,9 @@ export default [
 
     plugins: [
       // Allows node_modules resolution
-      resolve({
+      nodeResolve({
         extensions,
-        mainFields: ['jsnext:main, module, main'],
+        mainFields: ['module', 'main'],
       }),
 
       // Allow bundling cjs modules. Rollup doesn't understand cjs
@@ -55,9 +55,9 @@ export default [
 
       // Compile TypeScript/JavaScript files
       typescript({
-        clean: true,
         exclude: [ "node_modules", "*.d.ts", "**/*.d.ts" ],
         include: [ "*.ts+(|x)", "**/*.ts+(|x)", "*.m?js+(|x)", "**/*.m?js+(|x)" ],
+        module: "ESNext",
         tsconfig: "tsconfig.json",
         tslib: require('tslib'),
         typescript: require("typescript"),
@@ -68,6 +68,8 @@ export default [
         module: true,
         keep_classnames: true,
         keep_fnames: true,
+        compress: true,
+        mangle: true,
       }),
     ],
 
@@ -77,6 +79,7 @@ export default [
       exports: 'named',
       file: pkg.module,
       format: "es",
+      name: pkgName,
       sourcemap: true,
       globals,
     },
@@ -90,25 +93,37 @@ export default [
 
     plugins: [
       // Allows node_modules resolution
-      resolve({ extensions }),
+      nodeResolve({
+        extensions,
+        mainFields: ['main', 'node'],
+      }),
 
       // Allow bundling cjs modules. Rollup doesn't understand cjs
-      commonjs({ include: 'node_modules/**' }),
+      commonjs({
+        include: 'node_modules/**',
+        transformMixedEsModules: true,
+      }),
 
       // Compile TypeScript/JavaScript files
       typescript({
-        clean: true,
         exclude: [ "node_modules", "*.d.ts", "**/*.d.ts" ],
         include: [ "*.ts+(|x)", "**/*.ts+(|x)", "*.m?js+(|x)", "**/*.m?js+(|x)" ],
         tsconfig: "tsconfig.json",
-        module: "cjs",
         tslib: require('tslib'),
         typescript: require("typescript"),
       }),
 
+      babel({
+        extensions,
+        babelHelpers: 'bundled',
+        include: ["src/**/*"],
+        exclude: ["node_modules/**/*"],
+      }),
+
       terser({
-        keep_classnames: true,
-        keep_fnames: true,
+        ecma: 2021,
+        compress: true,
+        mangle: true,
       }),
     ],
 
@@ -118,6 +133,7 @@ export default [
       exports: 'named',
       file: pkg.main,
       format: 'cjs',
+      name: pkgName,
       sourcemap: true,
       globals,
     },
@@ -130,9 +146,22 @@ export default [
     external,
 
     plugins: [
-      resolve({ extensions }),
+      nodeResolve({
+        extensions,
+        mainFields: ['browser', 'main'],
+        browser: true,
+      }),
 
       commonjs({ include: 'node_modules/**' }),
+
+      // Compile TypeScript/JavaScript files
+      typescript({
+        exclude: [ "node_modules", "*.d.ts", "**/*.d.ts" ],
+        include: [ "*.ts+(|x)", "**/*.ts+(|x)", "*.m?js+(|x)", "**/*.m?js+(|x)" ],
+        tsconfig: "tsconfig.json",
+        tslib: require('tslib'),
+        typescript: require("typescript"),
+      }),
 
       babel({
         extensions,
@@ -141,7 +170,11 @@ export default [
         exclude: ["node_modules/**/*"],
       }),
 
-      terser(),
+      terser({
+        ecma: 2021,
+        compress: true,
+        mangle: true,
+      }),
     ],
 
     output: [{
@@ -163,14 +196,24 @@ export default [
     external,
 
     plugins: [
-      resolve({
+      nodeResolve({
         extensions,
+        mainFields: ['browser', 'main'],
         browser: true,
       }),
 
       commonjs({
         include: 'node_modules/**',
         transformMixedEsModules: true,
+      }),
+
+      // Compile TypeScript/JavaScript files
+      typescript({
+        exclude: [ "node_modules", "*.d.ts", "**/*.d.ts" ],
+        include: [ "*.ts+(|x)", "**/*.ts+(|x)", "*.m?js+(|x)", "**/*.m?js+(|x)" ],
+        tsconfig: "tsconfig.json",
+        tslib: require('tslib'),
+        typescript: require("typescript"),
       }),
 
       babel({
@@ -181,9 +224,9 @@ export default [
       }),
 
       terser({
-        module: false,
-        keep_classnames: true,
-        keep_fnames: true,
+        ecma: 2021,
+        compress: true,
+        mangle: true,
       }),
     ],
 
@@ -205,25 +248,21 @@ export default [
     output: [
       // CJS
       {
-        banner: bannerText,
         file: pkg.types,
         format: "es"
       },
       // UMD
       {
-        banner: bannerText,
         file: "./dist/umd/index.min.d.ts",
         format: "es"
       },
       // Browser
       {
-        banner: bannerText,
         file: "./dist/iife/index.min.d.ts",
         format: "es"
       },
       // Module
       {
-        banner: bannerText,
         file: "./dist/esm/index.min.d.ts",
         format: "es"
       },
