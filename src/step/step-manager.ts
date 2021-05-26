fb322a5721c8cfcb1114ea1ed81681ee5470c0d8
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2021-05-26 12:41:14 +0100

    improve step-manager documentation

--- src/step/step-manager.ts
+++ src/step/step-manager.ts
@@ -18,18 +18,29 @@ export interface StepManager {
 }
 
 function createPre(world: World) {
-  return function pre(): World {
+  /**
+   * Calls all system pre-update functions in order
+   * @returns the world
+   */
+  function pre(): World {
     world.getPreSystems().forEach((system) => system.pre(system.entities, world.global));
     return world;
-  };
+  }
+  return pre;
 }
 
 function createPost(world: World) {
-  return function post(int = 0): World {
+  /**
+   * Calls all system pre-update functions in order
+   * @param int the frame's interpolation alpha
+   * @returns the world
+   */
+  function post(int = 0): World {
     world.getPostSystems().forEach((system) => system.post(system.entities, world.global, int));
     world.refreshQueries();
     return world;
-  };
+  }
+  return post;
 }
 
 function createStep(maxUpdates: number, tempo: number, world: World) {
@@ -63,10 +74,16 @@ function createStep(maxUpdates: number, tempo: number, world: World) {
 }
 
 function createUpdate(world: World) {
-  return function update(dt = 0): World {
+  /**
+   * Call all system update functions in order
+   * @param dt frame delta time
+   * @returns the world
+   */
+  function update(dt = 0): World {
     world.getUpdateSystems().forEach((system) => system.update(system.entities, world.global, dt));
     return world;
-  };
+  }
+  return update;
 }
 
 export function createStepManager(world: World, spec: StepManagerSpec): StepManager {
