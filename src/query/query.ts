43ce8580a4ac5aa99546a8b925cbe1aee8b74217
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2022-03-14 17:51:04 +0000

    Minor improvements to query.ts

--- src/query/query.ts
+++ src/query/query.ts
@@ -13,26 +13,22 @@ export interface QuerySpec {
 }
 
 /** Queries are groupings of archetypes */
-export interface Query {
+export type Query = Readonly<{
   /** AND - Gather entities as long as they have all these components */
   all: Readonly<Component<unknown>[]>;
   /** OR - Gather entities as long as they have 0...* of these components */
   any: Readonly<Component<unknown>[]>;
   /** NOT - Gather entities as long as they don't have these components */
   none: Readonly<Component<unknown>[]>;
+}>;
+
+function _validateQueryArrays<T>(component: Component<T>) {
+  return Object.prototype.hasOwnProperty.call(component, "name");
 }
 
 export function isValidQuery(object: unknown): object is Query {
-  if (
-    !Array.isArray((object as Query).all) ||
-    !Array.isArray((object as Query).any) ||
-    !Array.isArray((object as Query).none)
-  ) {
-    return false;
-  }
-  const { any, all, none } = object as Query;
-  const _validateComponent = <T>(component: Component<T>) => Object.prototype.hasOwnProperty.call(component, "name");
-  return [...all, ...any, ...none].every(_validateComponent);
+  const { any = EMPTY_ARRAY, all = EMPTY_ARRAY, none = EMPTY_ARRAY } = object as Query;
+  return [...all, ...any, ...none].every(_validateQueryArrays);
 }
 
 /**
@@ -41,13 +37,12 @@ export function isValidQuery(object: unknown): object is Query {
  * @param spec.all AND - Gather entities as long as they have all these components
  * @param spec.any OR - Gather entities as long as they have 0...* of these components
  * @param spec.none NOT - Gather entities as long as they don't have these components
- * @returns a valid Query object
  */
-export function createQuery(spec: QuerySpec): Readonly<Query> {
+export function createQuery(spec: QuerySpec): Query {
   if (!spec) throw new SyntaxError("createQuery: specification object is required.");
   const { all = EMPTY_ARRAY, any = EMPTY_ARRAY, none = EMPTY_ARRAY } = spec;
-  if (![...all, ...any, ...none].every((component) => Object.prototype.hasOwnProperty.call(component, "name"))) {
-    throw new SyntaxError("Query specification object is invalid.");
+  if (![...all, ...any, ...none].every(_validateQueryArrays)) {
+    throw new SyntaxError("createQuery: Query specification object is invalid.");
   }
   return Object.freeze({
     all: Object.freeze([...all]),
