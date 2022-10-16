0ef1fc6f1f325950d40ab138bb5595dd623e60c7
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2022-10-16 16:53:26 +0100

    Update Bitfield.ts
    Based on Structurae (MIT) - https://github.com/zandaqo/structurae

--- src/constants.ts
+++ src/constants.ts
@@ -47,3 +47,39 @@ export const $_CHANGED = Symbol("changed");
 
 /** Symbol for use as a key for the `count` flag getter and setter */
 export const $_COUNT = Symbol("count");
+
+/** Lookup table for powers of 2 */
+export const LOG_2: Record<number, number> = Object.freeze({
+  1: 0,
+  2: 1,
+  4: 2,
+  8: 3,
+  16: 4,
+  32: 5,
+  64: 6,
+  128: 7,
+  256: 8,
+  512: 9,
+  1024: 10,
+  2048: 11,
+  4096: 12,
+  8192: 13,
+  16384: 14,
+  32768: 15,
+  65536: 16,
+  131072: 17,
+  262144: 18,
+  524288: 19,
+  1048576: 20,
+  2097152: 21,
+  4194304: 22,
+  8388608: 23,
+  16777216: 24,
+  33554432: 25,
+  67108864: 26,
+  134217728: 27,
+  268435456: 28,
+  536870912: 29,
+  1073741824: 30,
+  2147483648: 31,
+});
--- src/utils/bitfield.ts
+++ src/utils/bitfield.ts
@@ -1,5 +1,7 @@
 /* Copyright 2022 the Miski authors. All rights reserved. MIT license. */
 
+import { LOG_2 } from "../constants.js";
+
 /**
  * @note
  * `bit >>> 5` is used in place of `Math.floor(bit / 32)`.
@@ -8,14 +10,39 @@
 
 /** */
 export class Bitfield extends Uint32Array {
+  static get [Symbol.species](): Uint32ArrayConstructor {
+    return Uint32Array;
+  }
+
+  /** @returns the index of the least significant bit or -1 if error */
+  static getLsbIndex(value: number): number {
+    if (value === 2147483648) return 31;
+    return LOG_2[value & -value] ?? -1;
+  }
+
+  /** @returns the number of set bits in a given number */
+  static getSetBitCount(value: number): number {
+    const a = value - ((value >> 1) & 0x55555555);
+    const b = (a & 0x33333333) + ((a >> 2) & 0x33333333);
+    return (((b + (b >> 4)) & 0xf0f0f0f) * 0x1010101) >> 24;
+  }
+
   /**
    * Create a new Bitfield from an array of objects
-   * @param objs an array of objects which have an id number property
+   * @param length the number of bits in the bitfield
+   * @param key the key of the property to use for the bitfield's indexes
+   * @param objs an array of objects which have the key as an index to a number
+   *
+   * @example
+   * // Creating 32 bit bitfield from <T extends { id: number }>:
+   * Bitfield.fromObjects(32, "id", [{ id: 0, ... }, ...]);
    */
-  static fromIds<T extends { id: number }>(length: number, objs: T[]): Bitfield {
-    return objs.reduce((bitfield, { id }) => {
+  static fromObjects<T>(length: number, key: keyof T, objs: T[]): Bitfield {
+    return objs.reduce((bitfield, obj) => {
+      const id = obj[key] as number;
+      if (isNaN(id as number)) return bitfield;
       const i = Bitfield.indexOf(id);
-      if (i !== -1) bitfield[i] ^= 1 << (id - i * 32);
+      if (i > -1) bitfield[i] ^= 1 << (id  - i * 32);
       return bitfield;
     }, new Bitfield(length));
   }
@@ -31,8 +58,17 @@ export class Bitfield extends Uint32Array {
     return a & b;
   }
 
+  /**
+   * Creates a new Bitfield
+   * @param length the number of bits in the array
+   */
   constructor(length: number) {
     super(Math.ceil(length / 32));
+  }
+
+  /** @returns The amount of bits in the array */
+  get size(): number {
+    return this.length << 5;
   }
 
   /** @returns a new Bitfield with identical properties to this Bitfield */
@@ -43,10 +79,19 @@ export class Bitfield extends Uint32Array {
   }
 
   /** @returns a new Bitfield based on this one with toggled bits */
-  cloneWithToggle<T extends { id: number }>(sources: T[]): Bitfield {
+  cloneWithToggle<T>(key: keyof T, sources: T[]): Bitfield {
     const bitfield = this.clone();
-    sources.forEach(({ id }) => bitfield.toggle(id));
+    sources.forEach((source) => bitfield.toggle(source[key] as number));
     return bitfield;
+  }
+
+  /** @returns the index and position of a bit in the bitfield */
+  getPosition(bit: number): { index: number, position: number } {
+    const index = Bitfield.indexOf(bit);
+    return {
+      index,
+      position: bit - (index << 5),
+    }
   }
 
   /** @returns `true` if a given bit is 'on' (i.e., truthy) in the Bitfield */
@@ -59,12 +104,13 @@ export class Bitfield extends Uint32Array {
   /**
    * Toggle a bit in the Bitfield
    * @return the resulting state of the bit
+   * @throws if the bit is not found (i.e., indexOf(bit) === -1)
    */
   toggle(bit: number): boolean {
     const i = Bitfield.indexOf(bit);
     if (i === -1) throw new SyntaxError(`Bitfield.indexOf(${bit}) returned -1.`);
     if (this[i] === undefined) this[i] = 0;
     this[i] ^= 1 << (bit - i * 32);
-    return Boolean(this[i]! & (1 << (bit - i * 32)));
+    return !!(this[i]! & (1 << (bit - i * 32)));
   }
 }
