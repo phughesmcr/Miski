65df35d572f6dce9f87bbfa248dd99e8419a9793
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2022-10-19 15:09:11 +0100

    Update boxes demo

--- demo/boxes/demo.js
+++ demo/boxes/demo.js
@@ -4,48 +4,27 @@
 import { Component, Query, System, World } from "../../dist/miski.min.js";
 
 // Utility functions
-function rnd(a, b) {
-  return Math.random() * (b - a) + a;
-}
-
-function getRandomVelocity() {
-  return {
-    vx: rnd(-2, 2),
-    vy: rnd(-2, 2),
-  };
-}
-
-function getRandomPosition(w, h) {
-  return {
-    x: Math.random() * w,
-    y: Math.random() * h,
-  };
-}
-
-function getRandomShape() {
-   return {
-     primitive: Math.random() >= 0.5 ? 1 : 0,
-   };
-}
-
-const canvas = document.getElementsByTagName('canvas')[0];
-const ctx = canvas.getContext('2d', { alpha: false });
-ctx.imageSmoothingEnabled = false;
-
-const bRender = document.getElementById("bRender");
 const sEnts = document.getElementById("sEnts");
 const updateEntityCount = () => sEnts.textContent = NUM_ELEMENTS.toString();
+const rnd = (a, b) => Math.random() * (b - a) + a;
+const getRandomVelocity = () => ({ vx: rnd(-2, 2), vy: rnd(-2, 2) });
+const getRandomPosition = (w, h) => ({ x: Math.random() * w, y: Math.random() * h });
+const getRandomShape = () => ({ primitive: Math.random() >= 0.5 ? 1 : 0 }); // 0 = box, 1 = circle
 
 // Simulation constants
 const ENTITIES = [];
-const PRIMITIVES = { 0: "box", 1: "circle" };
 const TAU = 2 * Math.PI;
 const SHAPE_SIZE = 20;
 const SHAPE_HALF_SIZE = SHAPE_SIZE / 2;
 const MAX_CAPACITY = 5000;
-let INIT_ELEMENTS = 10;
+const INIT_ELEMENTS = 600;
 let NUM_ELEMENTS = 0;
-let SPEED_MULTIPLIER = 1;
+let SPEED_MULTIPLIER = 0.1;
+
+// Canvas
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d', { alpha: false });
+ctx.imageSmoothingEnabled = false;
 
 /* 1. Define components
  *
@@ -56,9 +35,9 @@ let SPEED_MULTIPLIER = 1;
  * Tags can be included in queries just like a regular component.
  * Tags and components can be tested for using `world.entityHasComponent(entity, component)`.
  */
-const cPosition = new Component({ name: "position", schema: { x: Float64Array, y: Float64Array }});
+const cPosition = new Component({ name: "position", schema: { x: Float32Array, y: Float32Array }});
 const cShape = new Component({ name: "shape", schema: { primitive: Uint8Array }});
-const cVelocity = new Component({ name: "velocity", schema: { vx: Float64Array, vy: Float64Array }});
+const cVelocity = new Component({ name: "velocity", schema: { vx: Float32Array, vy: Float32Array }});
 // a tag component:
 const tRenderable = new Component({ name: "renderable" });
 
@@ -78,11 +57,10 @@ const qRenderable = new Query({ all: [cPosition, cShape, tRenderable] });
 /* 3. Create a world and destructure functions
  *
  * To create a world you need two things:
- *  i. components: an array of components to register in the world (required)
- * ii. capacity: the maximum number of entities you want to allow in the world (default = 1_000_000)
+ *  i. components: an array of components to register in the world
+ * ii. capacity: the maximum number of entities you want to allow in the world
  *
  * Components cannot be registered after the world has been created.
- * The default maximum number of entities is 1 million but you should set this manually to avoid wasting memory.
  */
 const world = new World({
   capacity: MAX_CAPACITY,
@@ -94,7 +72,7 @@ const world = new World({
   ]
 });
 
-// Expose the world object for debugging (optional - don't do this in production!)
+// Expose the world object for debugging (don't do this in production!)
 window.world = world;
 
 /* 4. Define Systems
@@ -106,30 +84,28 @@ window.world = world;
  * Defining systems using `createSystem` isn't necessary but doing so ensures type safety.
  * Systems created using `createSystem` will return whatever your function returns (i.e., ReturnType<T>).
  */
-
-const prevX = new Array(MAX_CAPACITY).fill(0);
-const prevY = new Array(MAX_CAPACITY).fill(0);
-
-const sMovable = new System({ query: qMovable, system: (components, entities, delta, ctx) => {
-  const width = ctx.canvas.width;
-  const height = ctx.canvas.height;
+const sMovable = new System({ query: qMovable, system: (components, entities, delta) => {
+  const { width, height } = canvas;
   const { position, velocity } = components;
   const { x, y } = position;
   const { vx, vy } = velocity;
   for (let entity of entities) {
-    prevX[entity] = x[entity];
-    prevY[entity] = y[entity];
-    x[entity] += vx[entity] * SPEED_MULTIPLIER;
-    y[entity] += vy[entity] * SPEED_MULTIPLIER;
-    // wrapping
-    if (x[entity] > width) x[entity] = -SHAPE_SIZE;
-    if (x[entity] < -SHAPE_SIZE) x[entity] = width + SHAPE_SIZE;
-    if (y[entity] > height) y[entity] = -SHAPE_SIZE;
-    if (y[entity] < -SHAPE_SIZE) y[entity] = height + SHAPE_SIZE;
+    x[entity] += vx[entity] * delta * SPEED_MULTIPLIER;
+    y[entity] += vy[entity] * delta * SPEED_MULTIPLIER;
+    if (x[entity] > width + SHAPE_HALF_SIZE) {
+      x[entity] = -SHAPE_HALF_SIZE;
+    } else if (x[entity] < -SHAPE_HALF_SIZE) {
+      x[entity] = width + SHAPE_HALF_SIZE;
+    }
+    if (y[entity] > height + SHAPE_HALF_SIZE) {
+      y[entity] = -SHAPE_HALF_SIZE;
+    } else if (y[entity] < -SHAPE_HALF_SIZE) {
+      y[entity] = height + SHAPE_HALF_SIZE;
+    }
   }
-}});
+}}).init(world);
 
-const drawBox = (x, y, ctx) => {
+const drawCircle = (x, y) => {
   ctx.fillStyle = "#888";
   ctx.beginPath();
   ctx.arc(x, y, SHAPE_HALF_SIZE, 0, TAU, false);
@@ -139,7 +115,7 @@ const drawBox = (x, y, ctx) => {
   ctx.stroke();
 }
 
-const drawCircle = (x, y, ctx) => {
+const drawBox = (x, y) => {
   ctx.beginPath();
   ctx.rect(x - SHAPE_HALF_SIZE, y - SHAPE_HALF_SIZE, SHAPE_SIZE, SHAPE_SIZE);
   ctx.fillStyle= "#f28d89";
@@ -149,43 +125,48 @@ const drawCircle = (x, y, ctx) => {
   ctx.stroke();
 }
 
-const render = (components, entities, alpha, ctx) => {
+const sRender = new System({ query: qRenderable, system: (components, entities, alpha) => {
   const { position, shape } = components;
   const { x, y } = position;
   const { primitive } = shape;
   ctx.fillStyle = "#ffffff";
-  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
   for (let entity of entities) {
-    const dx = (prevX[entity]) ? (x[entity] - prevX[entity]) * alpha + prevX[entity] : x[entity];
-    const dy = (prevY[entity]) ? (y[entity] - prevY[entity]) * alpha + prevY[entity] : y[entity];
-    (PRIMITIVES[primitive[entity]] === "box") ? drawBox(dx, dy, ctx) : drawCircle(dx, dy, ctx);
+    if (primitive[entity] === 0) {
+      drawBox(x[entity], y[entity]);
+    } else {
+      drawCircle(x[entity], y[entity]);
+    }
   }
-}
-const sRender = new System({ system: render, query: qRenderable });
+}}).init(world);
 
-// You can define a prefab like so:
+// You can define a prefab factory like so:
 const shapeBuilder = world.addComponentsToEntity(cVelocity, cPosition, cShape, tRenderable);
 
 /* 5. Create Entities and give them some components */
-const createShapes = (n, w = ctx.width, h = ctx.height) => {
+const createShapes = (n, w = canvas.width, h = canvas.height) => {
+  while (NUM_ELEMENTS + n > world.capacity) {
+    n -= 1;
+  }
   for (let i = 0; i < n; i++) {
-    const shape = world.createEntity(); // this is the only Miski specific bit
-    if (shape !== undefined) {
+    const entity = world.createEntity(); // this is the only Miski specific bit
+    if (entity !== undefined) {
       NUM_ELEMENTS++;
-      shapeBuilder(shape, {
+      shapeBuilder(entity, {
         position: getRandomPosition(w, h),
         shape: getRandomShape(),
         velocity: getRandomVelocity(),
       });
-      ENTITIES.push(shape);
-    } else {
-      console.warn("NO MORE ENTITIES LEFT!");
+      ENTITIES.push(entity);
     }
   }
   updateEntityCount();
 }
 
 const destroyShapes = (n) => {
+  while (NUM_ELEMENTS - n < 0) {
+    n -= 1;
+  }
   for (let i = 0; i < n; i++) {
     const e = ENTITIES.pop();
     if (e !== undefined) {
@@ -197,7 +178,6 @@ const destroyShapes = (n) => {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  const sFPS = document.getElementById("sFPS");
   const sResX = document.getElementById("sResX");
   const sResY = document.getElementById("sResY");
   const rSpeed = document.getElementById("rSpeed");
@@ -205,21 +185,23 @@ window.addEventListener("DOMContentLoaded", () => {
   const bSub = document.getElementById("subtract");
   const bAddBig = document.getElementById("addBig");
   const bSubBig = document.getElementById("subtractBig");
+  const bRender = document.getElementById("bRender");
 
-  bAdd.addEventListener("click", () => { createShapes(10); });
-  bSub.addEventListener("click", () => { destroyShapes(10); });
-  bAddBig.addEventListener("click", () => { createShapes(100); });
-  bSubBig.addEventListener("click", () => { destroyShapes(100); })
+  bAdd.addEventListener("click", () => { createShapes(10); }, { passive: true });
+  bSub.addEventListener("click", () => { destroyShapes(10); }, { passive: true });
+  bAddBig.addEventListener("click", () => { createShapes(100); }, { passive: true });
+  bSubBig.addEventListener("click", () => { destroyShapes(100); }, { passive: true })
   rSpeed.addEventListener("input", () => { SPEED_MULTIPLIER = rSpeed.value; }, { passive: true });
   SPEED_MULTIPLIER = rSpeed.value;
 
   const resize = () => {
-    const { devicePixelRatio = 2, innerHeight, innerWidth } = window;
-    canvas.width = innerWidth; //* devicePixelRatio;
-    canvas.height = innerHeight //* devicePixelRatio;
-    canvas.style.width = innerWidth;
-    canvas.style.height = innerHeight;
-    // ctx.scale(devicePixelRatio, devicePixelRatio);
+    const { devicePixelRatio = 1 } = window;
+    const { clientHeight, clientWidth } = document.documentElement;
+    canvas.width = clientWidth * devicePixelRatio;
+    canvas.height = clientHeight * devicePixelRatio;
+    canvas.style.width = clientWidth + "px";
+    canvas.style.height = clientHeight + "px";
+    ctx.scale(devicePixelRatio, devicePixelRatio)
     sResX.textContent = canvas.width;
     sResY.textContent = canvas.height;
   }
@@ -227,34 +209,34 @@ window.addEventListener("DOMContentLoaded", () => {
   const fps = 60;
   const frameDuration = 1000 / fps;
   let lag = 0;
-  let previous = 0;
-  let startTime = null;
+  let previous;
 
   const start = (time) => {
-    if (startTime === null) startTime = time;
     requestAnimationFrame(start);
     let delta = time - previous;
-    if (delta > 1000) delta = frameDuration;
-    lag += delta;
-    if (lag >= frameDuration) {
-      // call your systems just like regular functions
-      sMovable.exec(world, delta, ctx);
-      lag -= frameDuration;
-    }
-    const alpha = lag / frameDuration;
-    // call your systems just like regular functions
-    if (bRender.checked) sRender.exec(world, alpha, ctx);
-
     previous = time;
-
+    if (delta > fps) delta = frameDuration;
+    lag += delta;
+    let updated = false;
+    while (lag >= frameDuration) {
+      // call your systems just like regular functions
+      sMovable(delta);
+      lag -= frameDuration;
+      updated = true;
+    }
+    if (updated) {
+      if (bRender.checked) sRender(lag / frameDuration);
+    }
     // runs World maintenance functions
     // I recommend calling this once per frame
     world.refresh();
   };
 
-  window.addEventListener("resize", resize, {passive: true});
   resize();
-  createShapes(INIT_ELEMENTS, canvas.width, canvas.height);
+  window.addEventListener("resize", resize, {passive: true});
+  createShapes(INIT_ELEMENTS);
   updateEntityCount();
+
+  previous = performance.now();
   requestAnimationFrame(start);
-}, false);
+}, { passive: true, once: true });
