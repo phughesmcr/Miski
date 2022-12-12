1e0e96af13e9c7437665fffab21ed8f69c2cf885
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2022-12-12 20:29:05 +0000

    Make sparse-facade.ts more readable

--- src/utils/sparse-facade.ts
+++ src/utils/sparse-facade.ts
@@ -4,22 +4,28 @@ import { BitPool } from "./bitpool.js";
 import type { TypedArray } from "./utils.js";
 import type { Entity } from "../world.js";
 
+type SparseFacade<T extends TypedArray> = T & {
+  get(entity: Entity): T[0] | undefined;
+  set(entity: Entity, value: T[0]): boolean;
+  delete(entity: Entity): boolean;
+};
+
 /**
  * @param dense the typed array to apply the facade to
  * @returns A proxy to the dense array
  */
-export function sparseFacade<T extends TypedArray>(dense: T): T {
+export function sparseFacade<T extends TypedArray>(dense: T): SparseFacade<T> {
   /** Map<Entity, Dense Array Index> */
-  const sparse: Map<Entity, number> = new Map();
+  const sparse = new Map<Entity, number>();
 
   /** Array of available indexes in dense */
   const available = new BitPool(dense.length);
 
   /** @returns the entity's value from the dense array or undefined if non-existant */
-  const _get = (entity: Entity) => dense[sparse.get(entity) ?? -1];
+  const get = (entity: Entity) => dense[sparse.get(entity) ?? -1];
 
   /** @returns `false` if dense array is full, `true` if value set successfully */
-  const _set = (entity: Entity, value: T[0]): boolean => {
+  const set = (entity: Entity, value: T[0]): boolean => {
     const idx = sparse.get(entity) ?? available.acquire();
     if (idx === undefined) return false;
     dense[idx] = value;
@@ -28,7 +34,7 @@ export function sparseFacade<T extends TypedArray>(dense: T): T {
   };
 
   /** @returns `false` if the entity isn't already stored, `true` if deleted successfully */
-  const _deleteProperty = (entity: Entity): boolean => {
+  const deleteProperty = (entity: Entity): boolean => {
     const idx = sparse.get(entity);
     if (idx === undefined) return false;
     dense[idx] = 0;
@@ -38,8 +44,8 @@ export function sparseFacade<T extends TypedArray>(dense: T): T {
   };
 
   return new Proxy(dense, {
-    get: (_target: T, key: string | symbol) => _get(key as unknown as Entity),
-    set: (_target: T, key: string | symbol, value: T[0]) => _set(key as unknown as Entity, value),
-    deleteProperty: (_target: T, key: string | symbol) => _deleteProperty(key as unknown as Entity),
-  });
+    get: (_target: T, key: string | symbol) => get(key as unknown as Entity),
+    set: (_target: T, key: string | symbol, value: T[0]) => set(key as unknown as Entity, value),
+    deleteProperty: (_target: T, key: string | symbol) => deleteProperty(key as unknown as Entity),
+  }) as SparseFacade<T>;
 }
